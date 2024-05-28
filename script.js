ba99d8fd02ef8c9d9172d0faa4a5404990da957e
GABRIEL NEPOMUCENO DE MENEZES <00001126525194sp@aluno.educacao.sp.gov.br>
2024-05-28 11:27:05 -0300

    atualização javascript

---
+++ script.js
@@ -0,0 +1,6 @@
+function somarNumeros(){
+    var numero1 = parseInt(document.getElementById("num1").value);
+    var numero2 = parseInt(document.getElementById("num2").value);
+    var resultado = numero1+numero2
+    alert(resultado)
+}
